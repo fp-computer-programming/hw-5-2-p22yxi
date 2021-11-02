9718a662517c7b2041cfcc33bc5a65e8d668195f
p22yxi <p22yxi@fairfieldprep.org>
2021-11-02 10:20:19 -0400

    1

--- hw-5-2.py
+++ hw-5-2.py
@@ -1,6 +1,45 @@
 # Author Ypngdong Xi (Oct 29 2021)
 
 import math
+pi = math.pi
+e = math.e
 
 # Question 1
-math.isqrt(529)
+anwser1 = math.isqrt(529)
+print(anwser1)
+
+# Quesrion 2
+anwser2 = math.sin(0)
+print(anwser2)
+
+# Quesrion 3
+anwser3 = math.cos(2 * pi)
+print(anwser1)
+
+# Question 4
+anwser4 = math.pow(2.5, 5)
+print(anwser4)
+
+# Question 5
+anwser5 = math.log(e)
+print(anwser5)
+
+# Question 6
+anwser6 = math.pow(e, 4)
+print(anwser6)
+
+# Question 7
+anwser7 = math.ceil(-12.9)
+print(anwser7)
+
+# Question 8
+anwser8 = math.floor(7.25)
+print(anwser8)
+
+# Question 9
+anwser9 = (2 * (math.log(1000)))
+print(anwser9)
+
+# Question 10
+anwser10 = math.radians(180)
+print(anwser10)
